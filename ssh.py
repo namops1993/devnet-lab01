5b1535ad1f6a5c2b1028e2cce3df533559c86867
namops1993 <namops1993@gmail.com>
2020-09-16 15:11:51 +0700

    Update GCB

---
+++ ssh.py
@@ -0,0 +1,22 @@
+import pexpect
+
+host = "localhost 30001"
+username = "cisco"
+password = "cisco"
+command = 'show ip int brief'
+
+session = pexpect.spawn('telnet ' + host, timeout=20)
+
+session.sendline('\r\n')
+
+result = session.expect(['Username:', pexpect.TIMEOUT])
+session.sendline(username)
+result = session.expect(['Password:', pexpect.TIMEOUT])
+session.sendline(password)
+result = session.expect(['>', pexpect.TIMEOUT])
+session.sendline('enable')
+result = session.expect(['#', pexpect.TIMEOUT])
+session.sendline(command)
+result = session.expect(['#', pexpect.TIMEOUT])
+print(session.before)
+
